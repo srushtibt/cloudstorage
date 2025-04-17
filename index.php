ca45afad80009eb211346a48a9f13ebf368d1cd3
srushtibt <srushti.bt@cmr.edu.in>
2025-04-17 12:22:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
-<?php include'connection.php' ?>
+<?php
+ob_start(); // Start output buffering to prevent "headers already sent" errors
+session_start(); // Start session before anything else
+include 'connection.php'; // Include DB connection
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
@@ -14,7 +17,6 @@
             margin: auto;
             background-color: white;
             margin-top: 10%;
-            background-color: white;
             padding: 20px;
             text-align: center;
         }
@@ -42,13 +44,9 @@
             height: 80px;
             width: 80px;
         }
-
     </style>
 </head>
-
 <body>
-
-
     <div id="log_div">
         <center>
             <div>
@@ -58,25 +56,20 @@
                             <td colspan="2" style="text-align:center;"><img id="img" src="images/avatar.png"></td>
                         </tr>
                         <tr>
-                            <td colspan="2">
-                                <h3>Username</h3>
-                            </td>
+                            <td colspan="2"><h3>Username</h3></td>
                         </tr>
                         <tr>
-                            <td colspan="2"><input type=text name="u" value=""></td>
+                            <td colspan="2"><input type="text" name="u" required></td>
                         </tr>
                         <tr>
-                            <td colspan="2">
-                                <h3>Password</h3>
-                            </td>
+                            <td colspan="2"><h3>Password</h3></td>
                         </tr>
                         <tr>
-                            <td colspan="2"><input type=password name="p" value=""></td>
+                            <td colspan="2"><input type="password" name="p" required></td>
                         </tr>
                         <tr>
-                            <td><br><input name="btn" type=submit value="Login" style="background-color:#62a26b;"></td>
-
-                            <td><br><input name="btn1" type=submit value="Register User" style="background-color:#7486d7;"></td>
+                            <td><br><input name="btn" type="submit" value="Login" style="background-color:#62a26b;"></td>
+                            <td><br><input name="btn1" type="submit" value="Register User" style="background-color:#7486d7;"></td>
                         </tr>
                     </table>
                 </form>
@@ -84,41 +77,47 @@
         </center>
     </div>
 </body>
-
 </html>
 
-<?php 
+<?php
+if (isset($_POST['btn'])) {
+    $type = "";
+    $user = $_POST['u'];
+    $pass = $_POST['p'];
+
+    // Store session user and time regardless of result
+    $_SESSION['user'] = $user;
+    $_SESSION['time'] = time();
+
+    // Query to find matching user
+    $result = mysqli_query($connection, "SELECT * FROM user WHERE user='$user' AND pass='$pass'");
     
-    if(isset($_POST['btn']))
-    {
-        $type="";
-$user=$_POST['u'];
-$pass=$_POST['p'];
-$_SESSION['user']=$user;
-$_SESSION['time']=time();
-$result=mysqli_query($connection,"select * from user where user='$user' and pass='$pass'");
-if(mysqli_num_rows($result)!=0)
-{
-    if($row=mysqli_fetch_assoc($result))
-    {
-        session_start();
-        $_SESSION['database_name']=$row['database_name'];
-        $_SESSION['user']=$user;
-        $_SESSION['active_time']=time();
-        $_SESSION['expire_time']=$_SESSION['active_time']+(15*60);
-        $type=$row['type'];
+    if (mysqli_num_rows($result) != 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['database_name'] = $row['database_name'];
+        $_SESSION['user'] = $user;
+        $_SESSION['active_time'] = time();
+        $_SESSION['expire_time'] = $_SESSION['active_time'] + (15 * 60); // 15 minutes session
+
+        $type = $row['type'];
+
+        // Redirect based on user type
+        if ($type == "user") {
+            header("Location: folder_access.php");
+            exit();
+        } elseif ($type == "admin") {
+            header("Location: admin_panel.php");
+            exit();
+        }
+    } else {
+        echo '<script>alert("Record not matched")</script>';
     }
-    if($type=="user")
-    header("location:folder_access.php");
-    elseif($type=="admin")
-        header("location:admin_panel.php");
-}
-else
-        echo'<script>alert("record not matched")</script>';
-    }
-if(isset($_POST['btn1']))
-{ 
-header("location:reg.php");
 }
 
+if (isset($_POST['btn1'])) {
+    header("Location: reg.php");
+    exit();
+}
+
+ob_end_flush(); // End output buffering and send output
 ?>
